83353c300129a26a5618d9739a3585449745ad17
Youngju <ahnyoungju@gmail.com>
2019-10-29 15:50:02 +1100

    Deleting Test

--- GetWeather.py
+++ GetWeather.py
@@ -10,6 +10,32 @@ response = requests.get(url)
 #print(response.text[:1000])
 #print( type(response.text))
 
+todayWeather = {}
+
 jsonFormat = json.loads(response.text)
 print( jsonFormat["list"][0]["weather"][0]["main"])
 print( jsonFormat["list"][0]["weather"][0]["description"])
+print( jsonFormat["list"][0]["main"]["temp_min"])
+print( jsonFormat["list"][0]["main"]["temp_max"])
+print( jsonFormat["list"][0]["main"]["humidity"])
+print( jsonFormat["list"][0]["wind"]["speed"])
+print( jsonFormat["city"]["sunrise"])
+print( jsonFormat["city"]["sunset"])
+
+todayWeather["Description"] = jsonFormat["list"][0]["weather"][0]["description"]
+todayWeather["Temp_min"] = jsonFormat["list"][0]["main"]["temp_min"]
+todayWeather["Temp_max"] = jsonFormat["list"][0]["main"]["temp_max"]
+todayWeather["Humidity"] = jsonFormat["list"][0]["main"]["humidity"]
+todayWeather["Wind_speed"] = jsonFormat["list"][0]["wind"]["speed"]
+todayWeather["Sunrise"] = jsonFormat["city"]["sunrise"]
+todayWeather["Sunset"] = jsonFormat["city"]["sunset"]
+
+sunrise = jsonFormat["city"]["sunrise"]
+timezone = jsonFormat["city"]["timezone"]
+sunset = jsonFormat["city"]["sunset"]
+print( todayWeather )
+
+# Write json to TodayWeather.json file
+fout = open("TodayWeather.json", "w")
+fout.write(str(todayWeather))
+fout.close()
