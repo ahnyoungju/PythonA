8ea86c73d6d025c4f5a2a4cfde879c5dae4b66a0
kumilieu <kumi.lieu@gmail.com>
2019-11-12 12:56:41 +1100

    Shopping list create

--- Kumi.py
+++ Kumi.py
@@ -1,5 +1,4 @@
-# provide menu for (1) adding items ADD, (2) assistance HELP, (3) print list SHOW (4) deleting items DELETE  commands
-
+# provide menu for (1) adding items ADD (2) assistance HELP (3) print list SHOW (4) deleting items DELETE  commands
 # provide list of all items
 
 shopping_list = []
@@ -12,7 +11,7 @@ def show_help():
 
   SHOPPING LIST MENU:
 
-  1. ADD : to add new items to shopping list
+  1. DONE : to add new items to shopping list and print list when done
   2. HELP : to bring up menu
   3. SHOW : to print list of current items
   4. DELETE : to delete items from shopping list
@@ -28,10 +27,8 @@ def show_list():
     # print current list of items
 
     print ("Your shopping list: ")
-
     for item in shopping_list:
         print (item)
-
 
 def add_item_to_list(new_item):
     # add new item to shopping list
@@ -47,16 +44,16 @@ while True:
     new_item = input("> ")
     # user have option to quit app
 
-    if new_item == "ADD":
+    if new_item == "1":
         show_list()
         break
-    elif new_item == "HELP":
-        show_help()
-        continue
-    elif new_item == "SHOW":
+    elif new_item == "2":
         show_list()
         continue
-	elif new_item == "DELETE":
+    elif new_item == "3":
+        show_help()
+        continue
+    elif new_item == "4":
         show_list()
         continue
 
